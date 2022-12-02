e393eb91f53cad00ee1e13fd22987533e606a0ad
jeffanjelo <jeffanjelorondilla@csus.edu>
2022-12-01 16:29:32 -0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -23,6 +23,8 @@ if($result->num_rows >0){
    while($row = $result->fetch_assoc()){
       echo "ID: " . $row["UID"]. " - Name: ". $row["Name"]. "<br>";
    }
+} else {
+   echo "0 results";
 }
 
 //prepare and bind new values
